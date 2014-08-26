62996214d177cab18f3a90e215ca2725f565e084
n.shubenkov <nshubenkov@NShubenkovs-MacBook-Pro.local>
2014-08-27 02:17:10 +0400

    first commit. started lifecycle describing

--- ViewControllerLifeCycle/ViewController.h
+++ ViewControllerLifeCycle/ViewController.h
@@ -9,7 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UILabel *labelNearButton;
+@property (weak, nonatomic) IBOutlet UISwitch *aSwitch;
+@property (weak, nonatomic) IBOutlet UILabel *labelNearSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *buttonPressed;
+@property (weak, nonatomic) IBOutlet UITextField *textInput;
+@property (weak, nonatomic) IBOutlet UIImageView *image;
 
+- (IBAction)buttonPressed:(UIButton *)sender;
 
 @end
 
